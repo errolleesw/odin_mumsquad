7283bf04c981aea4168a1a7d187e88c181af2340
Errol <62831245+errolleesw@users.noreply.github.com>
2023-04-27 09:31:02 +0800

    set up devise user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: 'johndoe@doe.com', password: 'johndoe')
+User.create(email: 'janedoe@doe.com', password: 'janedoe')
+User.create(email: 'errolleesw@gmail.com', password: 'errolleesw')
+
